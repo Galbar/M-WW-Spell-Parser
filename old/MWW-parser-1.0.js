96e1978b9ce12e3789b6f8fb99fb7daff3486d06
Alessio Linares Bernardini <alessio.linares2@gmail.com>
2013-12-15 18:48:24 +0100

    ....

--- old/MWW-parser-1.0.js
+++ old/MWW-parser-1.0.js
@@ -166,7 +166,7 @@ function ParseSpells(text)
 	return ret_text;
 }
 
-//var body = document.getElementsByTagName("body")[0];
-//body.innerHTML = ParseSpells(body.innerHTML);
+var body = document.getElementsByTagName("body")[0];
+body.innerHTML = ParseSpells(body.innerHTML);
 
-console.log(ParseSpells("his: EDQ and EQQ. H"));
+//console.log(ParseSpells("his: :EDQ: and EQQ. H"));
